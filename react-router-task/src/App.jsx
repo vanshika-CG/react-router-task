47bc11379b06435d723e8c9be92577e65503108e
vanshika <vanshika.jangamcg@gmail.com>
2025-03-30 22:58:02 +0530

    changed layout

--- react-router-task/src/App.jsx
+++ react-router-task/src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import './App.css'
+import './App.css';
 import { Routes, Route, Link } from "react-router-dom";
 import Meals from "./components/Meals";
 import Cocktails from "./components/Cocktails";
@@ -10,20 +10,30 @@ import CharacterDetails from "./components/CharacterDetails";
 function App() {
   return (
     <div>
-      <nav className="navbar" style={{ marginBottom: "10px" }}>
-        <div className="meal"><Link to="/" className="color">Meals</Link></div> 
-        <div className="meal2"><Link to="/cocktails" className="color">Cocktails</Link></div> 
-        <div className="meal1"><Link to="/harrypotter" className="color">Harry Potter Characters</Link></div>
-        <div className="meal3"><Link to="/indianbanks" className="color">Indian Banks</Link></div>
+      <nav className="navbar">
+        <div className="nav-item">
+          <Link to="/" className="color">Meals</Link>
+        </div> 
+        <div className="nav-item">
+          <Link to="/cocktails" className="color">Cocktails</Link>
+        </div> 
+        <div className="nav-item">
+          <Link to="/harrypotter" className="color">Harry Potter</Link>
+        </div>
+        <div className="nav-item">
+          <Link to="/indianbanks" className="color">Indian Banks</Link>
+        </div>
       </nav>
      
-      <Routes>
-        <Route path="/" element={<Meals />} />
-         <Route path="/cocktails" element={<Cocktails />} />
-        <Route path="/harrypotter" element={<HarryPotter />} />
-        <Route path="/indianbanks" element={<IndianBanks />} /> 
-        <Route path="/character/:name" element={<CharacterDetails />} />
-      </Routes>
+      <div className="content-container">
+        <Routes>
+          <Route path="/" element={<Meals />} />
+          <Route path="/cocktails" element={<Cocktails />} />
+          <Route path="/harrypotter" element={<HarryPotter />} />
+          <Route path="/indianbanks" element={<IndianBanks />} /> 
+          <Route path="/character/:name" element={<CharacterDetails />} />
+        </Routes>
+      </div>
     </div>
   );
 }
